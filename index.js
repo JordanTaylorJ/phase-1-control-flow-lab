1eddbb12a27c116c2cc91496e47b300a9fe982ee
JordanJoseph <jordant.joseph@outlook.com>
2022-01-02 19:28:44 -0700

    commit tree e12d3632c368fac976c947487f0abfe86d17afb8

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+    return "This one is on me!";
+  }
+  else if (feet > 2500){
+    return "No can do."
+  } 
+  else {
+    return "I will gladly take your thirty bucks."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let farewell;
+
+  switch (tip) {
+    case "generous":
+      farewell = "Thank you so much.";
+      break;
+    case "not as generous":
+      farewell = "Thank you.";
+      break;
+    default:
+      farewell = "Bye.";
+  }
+  return farewell;
 }
